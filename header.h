0f0b5e3420dc905ab92e742829f0c465b9b1bbf9
Slivka <roman.strelyaeff@gmail.com>
2023-11-29 14:03:46 +0300

    initial commit

---
+++ header.h
@@ -0,0 +1,34 @@
+#define староста main
+
+#define настя int
+#define варя char
+#define паша bool
+#define майя float
+#define рома *
+#define миша &
+#define даня double
+#define ильнар auto
+
+#define если if
+#define хуесли else
+
+#define мальчик true
+#define девочка false
+
+#define коротышка short
+#define без_негатива unsigned
+#define дылда long
+#define вечное const
+
+#define попробовать_отсидеть_пару try
+#define съебаться_с_пары catch
+
+#define практика class
+#define лекция struct
+
+#define вынуть cout
+#define вставить cin
+#define вернуться_домой return
+#define пенис "\n"
+
+#define Политех std
